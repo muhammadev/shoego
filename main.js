e5c6eb38696193b903aff8b4dc12aa64e23ac188
Muhammad Tarek <tarek3529@gmail.com>
2020-06-05 05:34:52 +0200

    added max-width 480px media query

--- main.js
+++ main.js
@@ -1,7 +1,24 @@
+let menu = document.querySelector(".menu-icon");
+let ulNav = document.querySelector(".ul-nav");
 let header = document.querySelector("header");
 let headerHeight = header.getBoundingClientRect().height;
 let homeHeight = document.querySelector("#home").getBoundingClientRect().height;
 let up = document.querySelector(".up");
+
+menu.addEventListener("click", function() {
+	if(ulNav.classList.contains("open-menu")) {
+		ulNav.classList.remove("open-menu")
+	} else {
+		ulNav.classList.add("open-menu")
+	}
+})
+
+ulNav.addEventListener("click", function() {
+	ulNav.classList.remove("open-menu")
+	// setTimeout(function() {
+	// 	ulNav.classList.remove("open-menu")
+	// }, 100)
+})
 
 document.addEventListener("scroll", () => {
 	if (window.pageYOffset + 50 > homeHeight) {
